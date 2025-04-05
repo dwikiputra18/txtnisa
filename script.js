fd4709f0008a5de313486df4a2ff2b37606c46b3
dwikiputra18 <dwikiputra678@gmail.com>
2025-04-05 22:29:30 +0700

    tambah tanya.html

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ function openEnvelope() {
   
     setTimeout(() => {
       document.getElementById('envelope').classList.add('show');
+  
+      setTimeout(() => {
+        document.getElementById('nextButton').style.display = 'block';
+      }, 1500);
     }, 100);
   }
+  function createFlower() {
+    const flower = document.createElement('div');
+    flower.classList.add('flower');
+    flower.style.left = Math.random() * 100 + 'vw';
+    flower.style.animationDuration = (Math.random() * 5 + 5) + 's'; // 5 - 10s
+    document.body.appendChild(flower);
+  
+    setTimeout(() => {
+      flower.remove();
+    }, 10000);
+  }
+  
+  setInterval(createFlower, 500);
   
